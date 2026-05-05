83374bee7b9da5c8e5faf84e0e6ac2a88c469566
aama012026 <aama012026@gmail.com>
2026-05-05 21:56:59 +0200

    started work on datastar actions

--- clarity.ts
+++ clarity.ts
@@ -6,6 +6,24 @@ console.log(`Clarity ver ${VERSION}`);
 const server = Bun.serve({
 	routes: {
 		'/': home, 
+		'/profile': (request, server) => {
+			const stream = new ReadableStream({
+				start(controller) {
+					controller.enqueue(patchSignals({
+						accountId: 86176724,
+						personaName: 'dotaDestroyer'
+					}))
+					controller.close()
+				},
+				cancel() {}
+			})
+			return new Response(stream, {
+				headers: {
+					"Content-Type": "text/event-stream",
+					"Cache-Control": "no-cache"
+				}
+			})
+		},
 		'/*': (request) => {
 			try {
 				return new Response(Bun.file(`./public/${new URL(request.url).pathname}`))
@@ -18,3 +36,18 @@ const server = Bun.serve({
 	}
 })
 console.log(`server running @ ${server.url}`)
+
+function patchSignals(data: Record<string, any>, onlyIfMissing: boolean = false): string {
+	return ( 
+		`event: datastar-patch-signals\n` + 
+		`data: onlyIfMissing ${onlyIfMissing}\n` +
+		`data: signals ${JSON.stringify(data)}\n\n`
+	)
+}
+function patchElements(data: string[], modifiers?: string[]) {
+	let ssEvent = `event: datastar-patch-elements\n`
+	modifiers?.forEach(mod => ssEvent += `data: ${mod}\n`)
+	data.forEach(line => ssEvent += `data: elements ${line}\n`)
+	ssEvent += '\n'
+	return ssEvent
+}
