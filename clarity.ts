d4ce3faa0b7400b7089d569cdc94e9ad84412c22
aama012026 <aama012026@gmail.com>
2026-05-05 23:12:30 +0200

    added createMatchSummary to server

--- clarity.ts
+++ clarity.ts
@@ -1,6 +1,9 @@
 const VERSION = 0.0
 
+import { heroLabels, type PlayerMatchSummary } from './src/modules/bindings';
+import { DIR, PATHS } from './src/modules/paths';
 import home from './src/pages/home.html'
+import { LEAVER_STATUS, leaverStatusByKey } from './src/types/OpenDotaTypes';
 
 console.log(`Clarity ver ${VERSION}`);
 const server = Bun.serve({
@@ -44,10 +47,62 @@ function patchSignals(data: Record<string, any>, onlyIfMissing: boolean = false)
 		`data: signals ${JSON.stringify(data)}\n\n`
 	)
 }
+
 function patchElements(data: string[], modifiers?: string[]) {
 	let ssEvent = `event: datastar-patch-elements\n`
 	modifiers?.forEach(mod => ssEvent += `data: ${mod}\n`)
 	data.forEach(line => ssEvent += `data: elements ${line}\n`)
 	ssEvent += '\n'
 	return ssEvent
+}
+
+function createMatchSummary(summary: PlayerMatchSummary) {
+	const {match, player, hero} = summary
+	const startTime = match.startTime ? new Date(match.startTime).toLocaleString() : 'unknown'
+	let result = 'Result unavailable'
+	let side = 'Side unavailable'
+	if(player.slot !== undefined) {
+		const playerTeam = player.slot < 128 ? 0 : 1
+		result = match.winningTeam === playerTeam ? 'Victory' : 'Defeat'
+		side = playerTeam === 0 ? 'Radiant' : 'Dire'
+
+	}
+	// TODO: make template function divide template into array by newlines.
+	const html = String.raw
+	return (
+		html`<tr class="match-summary" data-on:click="@get('/match/${match.id}')">
+			<td>
+				<div>Match: ${match.id}</div>
+				<div>Time: ${new Date(match.startTime!).toLocaleString()}<div>
+			</td>
+			<td><img src="${DIR.ROOT}/${PATHS.IMG.HEROES}/${heroLabels[hero.id]}"
+					alt="${heroLabels[hero.id]}"
+				>
+			</td>
+			<td>
+				<div data-result="${result}">${result}</div>
+				<div data-side="${side}">${side}</div>
+			</td>
+			<td>${timerStringFromSeconds(match.lengthSeconds)}</td>
+			<td>
+				<span>Kda: ${hero.kda.kills} / ${hero.kda.deaths} / ${hero.kda.assists}</span>
+			</td>
+			<td>
+				<div>${match.gameMode}</div>
+				<div>${match.lobbyType}</div>
+				${player.leaverStatus !== LEAVER_STATUS.NONE ? `<div>${leaverStatusByKey[player.leaverStatus]}</div>` : ''}
+			</td>
+		</tr>`
+	)
+
+
+}
+
+function timerStringFromSeconds(duration: number): string {
+	const wholeSeconds = Math.round(duration);
+	const seconds = wholeSeconds % 60;
+	const minutes = ((wholeSeconds - seconds) % 3600) / 60;
+	const hours = Math.floor((wholeSeconds - seconds - minutes) / 3600);
+	const hoursString = hours > 0 ? `${hours.toString().padStart(2, '0')}:` : '';
+	return `${hoursString}${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
 }
