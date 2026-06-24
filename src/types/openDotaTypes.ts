b90912a8cc2bd91b11f60b1ac63536689f7e9174
aama012026 <aama012026@gmail.com>
2026-06-24 12:55:58 +0200

    Fix: correct some interfaces

--- src/types/openDotaTypes.ts
+++ src/types/openDotaTypes.ts
@@ -92,7 +92,7 @@ export interface PlayerHeroStatsDTO extends PeerBaseDTO {
 	hero_id:number
 }
 // /:accountId/peers
-export interface PeerDTO extends PeerBaseDTO {
+export interface PeerDTO extends PeerBaseDTO, Pick<AvatarDTO, 'avatar'|'avatarfull'> {
 	account_id:AccountId,
 	with_gpm_sum:number|null,
 	with_xpm_sum:number|null,
@@ -101,8 +101,6 @@ export interface PeerDTO extends PeerBaseDTO {
 	is_contributor:boolean,
 	is_subscriber:boolean,
 	last_login:string|null,
-	avatar:string|null,
-	avatarfull:string|null
 }
 // /:accountId/pros
 export interface RelationalProPlayerDTO extends Omit<
@@ -199,8 +197,8 @@ export interface TeamMatchDTO extends MatchBaseDTO, MatchScoreDTO, TeamMatchDeta
 // GET /heroes/:heroId/matches
 export type HeroMatchDTO = MatchBaseDTO & KDAProps & TeamMatchDetailsDTO
 // GET /matches/:MatchId
-export interface SparseMatchDTO extends MatchSummaryDTO, MatchScoreDTO, MatchSeriesDTO {
-	players: SparsePlayerDTO[],
+export interface UnparsedMatchDTO extends MatchSummaryDTO, MatchScoreDTO, MatchSeriesDTO {
+	players: UnparsedPlayerDTO[],
 	cluster: number, // seen in dota constants
 	replay_salt?: number,
 	pre_game_duration: number, // not present in documentation.
@@ -219,10 +217,11 @@ export interface SparseMatchDTO extends MatchSummaryDTO, MatchScoreDTO, MatchSer
 	metadata: any, // not present in documentation
 	replay_url?: string,
 	patch:PatchId, // patch ID from dotaconstants
-	region:RegionId, // region ID from dotaconstants
+	//not present on anonymous matches
+	region?:RegionId, // region ID from dotaconstants
 }
 
-export interface ParsedMatchDTO extends SparseMatchDTO {
+export interface ParsedMatchDTO extends UnparsedMatchDTO {
 	players: ParsedPlayerDTO[],
 	teamfights?: TeamfightDTO[] | null,
 	pauses?: PauseDTO[], // unverified - empty in seen parsed matches
@@ -234,12 +233,23 @@ export interface ParsedMatchDTO extends SparseMatchDTO {
 	draft_timings?: DraftTimingDTO[], // present but empty in parsed matches. Prob deprecated.
 	all_word_counts?: Record<string, number>, // seen, but only empty
 	my_word_counts?: Record<string, number>, // seen, but only empty
-	comeback?: number, // max gold disadv. on winning team
 	stomp?: number,  // undocumented, prob max gold adv. on winning team (see win).
+	comeback?: number, // max gold disadv. on winning team
+	win?: number, // max gold advantage on winning team
+	throw?: number, // max gold adv. on losing team
+	loss?: number, // max gold disadvantage on losing team
 	// Present on league matches
-	radiant_team: TeamInfoDTO, //Only need to keep id.
-	dire_team: TeamInfoDTO, // Only need to keep id.
-	league: LeagueDTO, // Only need to keep id.
+	radiant_team_id?:number,
+	radiant_name?:string,
+	radiant_logo?:number,
+	radiant_team_complete?:number,
+	dire_team_id?:number,
+	dire_name?:string,
+	dire_logo?:number,
+	dire_team_complete?:number,
+	radiant_team?: TeamInfoDTO, //Only need to keep id.
+	dire_team?: TeamInfoDTO, // Only need to keep id.
+	league?: LeagueDTO, // Only need to keep id.
 	// Prob present when captains mode
 	radiant_captain?:AccountId,
 	dire_captain?:AccountId
@@ -247,9 +257,6 @@ export interface ParsedMatchDTO extends SparseMatchDTO {
 	// negative_votes: number,
 	// positive_votes: number[],
 	// skill: number | null, // bracket assigned by Valve (Normal, High, Very High)
-	// throw: number, // max gold adv. on losing team
-	// loss: number, // max gold disadvantage on losing team
-	// win: number, // max gold advantage on winning team
 }
 
 export interface ChatMsgDTO {
@@ -317,11 +324,7 @@ export interface PickBanDTO {
 	team: number,
 	order: number
 }
-
-export interface SparsePlayerDTO extends PlayerSummaryDTO, GameFormatDTO, OdRelation, PlayerRankDTO, MatchCountDTO {
-	party_id?:number|null,
-	team_number:number, // undocumented, prob unneeded - 0 for radiant and 1 for dire
-	team_slot:number, // undocumented, prob unneeded (0-4)
+export interface InventoryDTO {
 	item_0:number,
 	item_1:number,
 	item_2:number,
@@ -333,6 +336,13 @@ export interface SparsePlayerDTO extends PlayerSummaryDTO, GameFormatDTO, OdRela
 	backpack_2:number,
 	item_neutral:number, // artifact
 	item_neutral2:number, // enchantment
+}
+export interface UnparsedPlayerDTO extends
+PlayerSummaryDTO, GameFormatDTO, OdRelation,
+Partial<PlayerRankDTO>, Partial<AccountBaseDTO>, MatchCountDTO, InventoryDTO {
+	party_id?:number|null,
+	team_number:number, // undocumented, prob unneeded - 0 for radiant and 1 for dire
+	team_slot:number, // undocumented, prob unneeded (0-4)
 	permanent_buffs?:PermaBuffDTO[],
 	last_hits:number,
 	denies:number,
@@ -349,11 +359,15 @@ export interface SparsePlayerDTO extends PlayerSummaryDTO, GameFormatDTO, OdRela
 	gold:number, // @ match conclusion
 	gold_spent:number,
 	ability_upgrades_arr:number[],
+	total_gold:number,
+	total_xp:number,
+	kills_per_min:number,
+	kda:number,
+	abandons:number,
+	benchmarks:PlayerHeroPerformanceDTO,
 	// Composed from profile?
-	account_id?:AccountId,
-	personaname:string|null,
-	last_login:string|null, //<date-time>
 	name:string|null,
+	// match dupes?
 	radiant_win:boolean|null,
 	start_time:UnixTimestamp,
 	duration:number,
@@ -361,173 +375,168 @@ export interface SparsePlayerDTO extends PlayerSummaryDTO, GameFormatDTO, OdRela
 	patch:number,
 	region:number,
 	isRadiant:boolean,
-	total_gold:number,
-	total_xp:number,
-	kills_per_min:number,
-	kda:number,
-	abandons:number,
-	benchmarks:PlayerHeroPerformanceDTO,
 }
 
-export interface ParsedPlayerDTO extends SparsePlayerDTO {
-	obs_placed: number,
-	sen_placed: number,
-	creeps_stacked: number,
-	camps_stacked: number,
-	rune_pickups: number,
-	firstblood_claimed: number,
-	teamfight_participation: number, // rate? (0-1)
-	towers_killed: number,
-	roshans_killed: number,
-	observers_placed: number, // duplicate of obs_placed?
-	stuns: number, // seconds of all stuns for all players? (according to doc)
-	max_hero_hit: MaxHeroHitDTO, // highest dmg. instance player inflicted
-	times: number[], // moment in seconds other arrays' entries represent
-	gold_t: number[], // gold @ different timings
-	lh_t: number[], // @ each min. of game
-	dn_t: number[], // denies @ different times of the match
-	xp_t: number[], // xp @ min.i
-	obs_log: WardLogEntryDTO[],
-	obs_left_log: WardLogEntryDTO[], // When observer left - either killed or timed out
-	sen_log: WardLogEntryDTO[],
-	sen_left_log: WardLogEntryDTO[],
-	purchase_log: PurchaseDTO[],
-	kills_log: TimingDTO[],
-	buyback_log: BuybackDTO[],
-	runes_log: TimingDTO[],
-	connection_log: ConnectionEventDTO[],
-	lane_pos: Record<xPos, Record<yPos, number>>, //outer record key is x, inner y (or other way around), and value of inner is presumably weight.
-	obs: Record<xPos, Record<yPos, number>>
-	sen: Record<xPos, Record<yPos, number>>,
-	actions: Record<UnitOrderId, number>,
-	pings: number,
-	purchase: Record<string, number>,
-	gold_reasons: Record<GoldReasonId, number>,
-	xp_reasons: Record<XpReasonId, number>,
-	killed: Record<string, number>,
-	item_uses: Record<string, number>,
-	ability_uses: Record<string, number>,
-	ability_targets: Record<string, Record<string, number>>, // Record<abilityName, Record<targetName, count>>
-	damage_targets: Record<string, Record<string, number>>, // Record<damageSource, Record<targetName, amount>>
-	hero_hits: Record<string, number>, // Record<source, count>
-	damage: Record<string, number>, //Record<target, amount>
-	damage_taken: Record<string, number>, //Record<source, amount>
-	damage_inflictor: Record<string, number>, // Record<source, amount>
-	damage_inflictor_received: Record<string, number>, //Record<source, amount>
-	runes: Record<number, number>, // Record<Rune id, count>
-	killed_by: Record<string, number>, // Record<source, count>
-	kill_streaks: Record<number, number>, // Record<killCount, occurenceCount> ??
-	multi_kills: Record<number, number>, //Record<killCount, occurenceCount> ?? one of these are wrong.
-	life_state: Record<number, number>, //??
-	healing: Record<string, number>,
-	randomed: boolean,
-	pred_vict: boolean,
-	neutral_tokens_log: TimingDTO[], //prob. deprecated since replaced by madstones
-	neutral_item_history: NeutralItemHistoryDTO[]
-	neutral_kills: number,
-	tower_kills: number,
-	courier_kills: number,
-	lane_kills: number,
-	hero_kills: number,
-	observer_kills: number,
-	sentry_kills: number,
-	roshan_kills: number,
-	necronomicon_kills: number,
-	ancient_kills: number,
-	buyback_count: number,
-	observer_uses: number,
-	sentry_uses: number,
-	lane_efficiency: number, // rate - not rounded
-	lane_efficiency_pct: number, // percent - rounded to nearest.
-	lane: number | null, // which lane the hero laned in (presumably 0-2 or 1-3 (was 3 for safe on dire))
-	lane_role: number | null, // was 1 for carry on dire.
-	is_roaming: boolean | null,
-	purchase_time: Record<string, number>, // Record<itemName, moment>. Moment can be negative for pre-match start.
-	first_purchase_time: Record<string, number>, // Presumably same as purchase_time but without dup. keys.
-	item_win: Record<string, number>, // 1 for all items purchased in seen response.
-	item_usage: Record<string, number>, //(saw 1 for a set of tangos, quelling blade and magic wand...)??
-	purchase_tpscroll: number, // prob. count.
-	actions_per_min: number,
-	life_state_dead: number, // !! seconds spent dead !!
-	cosmetics: CosmeticDTO[],
-	// Not present ---------------------------------------------------------
-	// match_id: MatchId, // never seen, is present in match data.
-	additional_units?: object[] | null, // never seen, might be included when needed
+export interface ParsedPlayerDTO extends UnparsedPlayerDTO {
+	obs_placed:number,
+	sen_placed:number,
+	creeps_stacked:number,
+	camps_stacked:number,
+	rune_pickups:number,
+	firstblood_claimed:number,
+	teamfight_participation:number, // rate? (0-1)
+	towers_killed:number,
+	roshans_killed:number,
+	observers_placed:number, // duplicate of obs_placed?
+	stuns:number, // seconds of all stuns for all players? (according to doc)
+	max_hero_hit:MaxHeroHitDTO, // highest dmg. instance player inflicted
+	times:number[], // moment in seconds other arrays' entries represent
+	gold_t:number[], // gold @ different timings
+	lh_t:number[], // @ each min. of game
+	dn_t:number[], // denies @ different times of the match
+	xp_t:number[], // xp @ min.i
+	obs_log:WardLogEntryDTO[],
+	obs_left_log:WardLogEntryDTO[], // When observer left - either killed or timed out
+	sen_log:WardLogEntryDTO[],
+	sen_left_log:WardLogEntryDTO[],
+	purchase_log:PurchaseDTO[],
+	kills_log:TimingDTO[],
+	buyback_log:BuybackDTO[],
+	runes_log:TimingDTO[],
+	connection_log:ConnectionEventDTO[],
+	lane_pos:Record<xPos, Record<yPos, number>>, //outer record key is x, inner y (or other way around), and value of inner is presumably weight.
+	obs:Record<xPos, Record<yPos, number>>
+	sen:Record<xPos, Record<yPos, number>>,
+	actions:Record<UnitOrderId, number>,
+	pings:number,
+	purchase:Record<string, number>,
+	gold_reasons:Record<GoldReasonId, number>,
+	xp_reasons:Record<XpReasonId, number>,
+	killed:Record<string, number>,
+	item_uses:Record<string, number>,
+	ability_uses:Record<string, number>,
+	ability_targets:Record<string, Record<string, number>>, // Record<abilityName, Record<targetName, count>>
+	damage_targets:Record<string, Record<string, number>>, // Record<damageSource, Record<targetName, amount>>
+	hero_hits:Record<string, number>, // Record<source, count>
+	damage:Record<string, number>, //Record<target, amount>
+	damage_taken:Record<string, number>, //Record<source, amount>
+	damage_inflictor:Record<string, number>, // Record<source, amount>
+	damage_inflictor_received:Record<string, number>, //Record<source, amount>
+	runes:Record<number, number>, // Record<Rune id, count>
+	killed_by:Record<string, number>, // Record<source, count>
+	kill_streaks:Record<number, number>, // Record<killCount, occurenceCount> ??
+	multi_kills:Record<number, number>, //Record<killCount, occurenceCount> ?? one of these are wrong.
+	life_state:Record<number, number>, //??
+	healing:Record<string, number>,
+	randomed:boolean,
+	pred_vict:boolean,
+	neutral_tokens_log:TimingDTO[], //prob. deprecated since replaced by madstones
+	neutral_item_history:NeutralItemHistoryDTO[]
+	neutral_kills:number,
+	tower_kills:number,
+	courier_kills:number,
+	lane_kills:number,
+	hero_kills:number,
+	observer_kills:number,
+	sentry_kills:number,
+	roshan_kills:number,
+	necronomicon_kills:number,
+	ancient_kills:number,
+	buyback_count:number,
+	observer_uses:number,
+	sentry_uses:number,
+	lane_efficiency:number, // rate - not rounded
+	lane_efficiency_pct:number, // percent - rounded to nearest.
+	lane:number|null, // which lane the hero laned in (presumably 0-2 or 1-3 (was 3 for safe on dire))
+	lane_role:number|null, // was 1 for carry on dire.
+	is_roaming:boolean|null,
+	purchase_time:Record<string, number>, // Record<itemName, moment>. Moment can be negative for pre-match start.
+	first_purchase_time:Record<string, number>, // Presumably same as purchase_time but without dup. keys.
+	item_win:Record<string, number>, // 1 for all items purchased in seen response.
+	item_usage:Record<string, number>, //(saw 1 for a set of tangos, quelling blade and magic wand...)??
+	purchase_tpscroll:number, // prob. count.
+	actions_per_min:number,
+	life_state_dead:number, // !! seconds spent dead !!
+	cosmetics:CosmeticDTO[],
+	match_id?:MatchId,
+	additional_units?:AdditionalUnit[]|null,
 }
-
+export interface AdditionalUnit extends InventoryDTO {
+	unitname:string,
+}
 export interface PermaBuffDTO {
-	permanent_buff: number,
-	stack_count: number, // 0 for buffs without stacks
-	grant_time: number
+	permanent_buff:number,
+	stack_count:number, // 0 for buffs without stacks
+	grant_time:number
 }
 
 export interface BenchmarkPerformanceDTO {raw: number, pct: number}
-
 export interface PlayerHeroPerformanceDTO {
-	gold_per_min: BenchmarkPerformanceDTO,
-	xp_per_min: BenchmarkPerformanceDTO,
-	kills_per_min: BenchmarkPerformanceDTO,
-	last_hits_per_min: BenchmarkPerformanceDTO,
-	hero_damage_per_min: BenchmarkPerformanceDTO,
-	hero_healing_per_min: BenchmarkPerformanceDTO,
-	tower_damage: BenchmarkPerformanceDTO
+	gold_per_min:BenchmarkPerformanceDTO,
+	xp_per_min:BenchmarkPerformanceDTO,
+	kills_per_min:BenchmarkPerformanceDTO,
+	last_hits_per_min:BenchmarkPerformanceDTO,
+	hero_damage_per_min:BenchmarkPerformanceDTO,
+	hero_healing_per_min:BenchmarkPerformanceDTO,
+	tower_damage:BenchmarkPerformanceDTO
 }
 
 export interface MaxHeroHitDTO extends ChatMsgDTO {
-	unit: string, // redundant unless other than hero (maybe summon etc.)
-	value: number,
-	inflictor: string, // can prob be right-click, ability, item, etc.
-	max: boolean // unneccesary
+	unit:string, // redundant unless other than hero (maybe summon etc.)
+	value:number,
+	inflictor:string, // can prob be right-click, ability, item, etc.
+	max:boolean // unneccesary
 }
 
 export interface WardLogEntryDTO extends ChatMsgDTO {
-	attackername?: string,
-	x: number,
-	y: number,
-	z: number,
-	entityleft: boolean,
-	ehandle: number // same for corresponding wards in log and left_log arrays, might be useful if order of wards placed and wards left is different.
+	attackername?:string,
+	x:number,
+	y:number,
+	z:number,
+	entityleft:boolean,
+	ehandle:number // same for corresponding wards in log and left_log arrays, might be useful if order of wards placed and wards left is different.
 }
 
 export interface BuybackDTO {time:number, slot:number, player_slot:PlayerSlot}
 
 export interface ConnectionEventDTO {
-	time: number,
-	event: string,
-	player_slot: PlayerSlot | null
+	time:number,
+	event:string,
+	player_slot:PlayerSlot|null
 }
 
-export interface TimingDTO {time: number, key: string}
-
-export interface PurchaseDTO extends TimingDTO {charges: number}
+export interface TimingDTO {time:number, key:string}
+export interface PurchaseDTO extends TimingDTO {charges:number}
 
 export interface CosmeticDTO {
-	item_id: number, // unsure if this is ingame item id or id for cosmetic.
-	name: string | null,
-	prefab: string,
-	creation_date: string | null, // <date-time> (guessing iso8601)
-	image_inventory: string | null,
-	image_path: string | null,
-	item_description: string | null,
-	item_name: string,
-	item_rarity: string | null,
-	item_type_name: string | null,
-	used_by_heroes: string | null
+	item_id:number, // unsure if this is ingame item id or id for cosmetic.
+	name:string|null,
+	prefab:string,
+	creation_date:string|null, // <date-time> (guessing iso8601)
+	image_inventory:string|null,
+	image_path: string|null,
+	item_description:string|null,
+	item_name:string,
+	item_rarity:string|null,
+	item_type_name:string|null,
+	used_by_heroes:string|null
 }
 
 export interface NeutralItemHistoryDTO {
-	item_neutral: string, // check dotaconstants
-	time: number,
-	item_neutral_enhancement: string // check dotaconstants
+	item_neutral:string, // check dotaconstants
+	time:number,
+	item_neutral_enhancement:string // check dotaconstants
 }
 
 // TODO: verify pause shape.
-export interface PauseDTO {
-	time: number, // paused @ second
-	duration: number // in seconds
-}
-
+export interface PauseDTO {time:number,	duration:number}
 // GET /distributions
+export interface RankRowDTO {
+	bin:RankBitmask,
+	bin_name:RankBitmask, // duplicate info? (16.4.26)
+	count:number,
+	cumulative_sum:number
+}
 export interface RankDistDTO {
 	ranks: {
 		rows: RankRowDTO[],
@@ -535,12 +544,7 @@ export interface RankDistDTO {
 	}
 }
 
-export interface RankRowDTO {
-	bin: RankBitmask,
-	bin_name: RankBitmask, // duplicate info? (16.4.26)
-	count: number,
-	cumulative_sum: number
-}
+
 // GET /search
 export interface SearchResDTO {
 	account_id: AccountId,
@@ -560,6 +564,7 @@ export interface RankingDTO extends ProfileBaseDTO {
 	rank_tier: RankBitmask|null
 }
 // GET /benchmarks?hero_id=ID
+export interface PercentileDTO {percentile: number, value: number|null}
 export interface HeroBenchmark {
 	hero_id: number,
 	result: {
@@ -572,10 +577,7 @@ export interface HeroBenchmark {
 		tower_damage: PercentileDTO[],
 	}
 }
-export interface PercentileDTO {percentile: number, value: number|null}
-
 // GET /heroes
-
 // /heroes/:heroId/matchups
 export interface MatchupDTO extends WinrateLongDTO {hero_id: number}
 // /heroes/:heroId/durations
@@ -600,7 +602,6 @@ export interface LeagueDTO {
 	tier:string,
 	name:string
 }
-
 // GET /teams | /teams/:teamId
 export type TeamDTO = TeamInfoDTO & TeamStatsDTO
 export interface TeamInfoDTO {
@@ -609,7 +610,7 @@ export interface TeamInfoDTO {
 	tag:string,
 	logo_url:string
 }
-export interface TeamStatsDTO extends TeamInfoDTO {
+export interface TeamStatsDTO {
 	rating:number, //ELO rating.
 	delta:number|null, // Maybe ELO delta over a period?
 	wins:number,
@@ -617,7 +618,6 @@ export interface TeamStatsDTO extends TeamInfoDTO {
 	last_match_time:UnixTimestamp,
 	match_id:number|null, // Maybe id of last played match?
 }
-
 // /teams/:teamId/players
 export interface TeamPlayerDTO extends WinrateLongDTO {
 	account_id:AccountId,
@@ -774,4 +774,3 @@ export type ObjectiveType = typeof OBJECTIVE_TYPES[keyof typeof OBJECTIVE_TYPES]
 export const RADIANT_SLOTS = [0, 1, 2, 3, 4] as const
 export const DIRE_SLOTS = [128, 129, 130, 131, 132] as const
 export type PlayerSlot = typeof RADIANT_SLOTS[number] | typeof DIRE_SLOTS[number]
-
